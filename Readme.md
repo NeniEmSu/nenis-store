5843283224d66fe80d1d4e603409a1c3f9c0760b
Emmanuel Neni <emmanuelneni@gmail.com>
2020-10-06 17:34:35 +0300

    displaying products on index.js

--- Readme.md
+++ Readme.md
@@ -52,17 +52,17 @@ We'll be using:
 - [x] Create product table migration
 - [x] Seed product table with sample data
 - [x] Add api folder and create/mount router
-- [ ] Connect to the database
+- [x] Connect to the database
   - [x] Create database connection file
   - [x] Create a queries file
 - [x] List all records with GET /api/v1/products
   - [x] Create query
   - [x] Create route
-- [ ] List all records in /index.html
-  - [ ] AJAX Request to GET /products
-  - [ ] Append to DOM
-  - [ ] Each product links to /product.html?id=:id
-  - [ ] Display a link to /create.html
+- [x] List all records in /index.html
+  - [x] AJAX Request to GET /products
+  - [x] Append to DOM
+  - [x] Each product links to /product.html?id=:id
+  - [x] Display a link to /create.html
 - [ ] Show one record with GET /api/v1/products/:id
   - [ ] Validate id
   - [ ] Create query
--- client/index.js
+++ client/index.js
@@ -1,7 +1,29 @@
-const API_URL = 'http://localhost:3000'
+const API_URL = 'http://localhost:3000/api/v1/products'
+const productsSection = document.querySelector('main section')
 
+getProducts()
+  .then(displayProducts)
 
-fetch(API_URL)
-  .then(res => res.json())
-  .then(result => console.log(result))
-  .catch(error => console.error(error))
+function getProducts() {
+  return fetch(API_URL)
+    .then(res => res.json())
+}
+
+function displayProducts({
+  products
+}) {
+  products.forEach(product => {
+    const productArticle = document.createElement('article');
+    productsSection.appendChild(productArticle)
+    productArticle.outerHTML = `
+    <article class="card col-sm-4">
+      <img class="card-img-top" src="${product.image}" alt="${product.title}">
+      <div class="card-body">
+        <h5 class="card-title">${product.title}</h5>
+        <p class="card-text">${product.description}</p>
+        <a href="/product.html?id=${product.id}" class="btn btn-primary">View Product</a>
+      </div>
+    </article>
+    `
+  });
+}
