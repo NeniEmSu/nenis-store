824b457980b80f83666f22028190a5ead6ce0258
Emmanuel Neni <emmanuelneni@gmail.com>
2020-10-07 14:16:09 +0300

    updating routes

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const productsCtrl = require('../controllers/productsController');
+const productsCtrl = require("../controllers/productsController");
 
-router.get('/products', productsCtrl.getProducts)
+router.get("/", productsCtrl.getProducts);
 
-router.get('/product/:id', productsCtrl.getSingeProduct)
+router.get("/:id", productsCtrl.getSingeProduct);
 
-module.exports = router
+module.exports = router;
