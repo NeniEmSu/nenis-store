21d8ba3fb6b694632385dc7449c73c115b9a629a
Emmanuel Neni <emmanuelneni@gmail.com>
2020-10-08 17:09:45 +0300

    moving body validtion to middleware

---
+++ server/middleware/validateBody.js
@@ -0,0 +1,28 @@
+function isValidProduct(product) {
+  let returnObj = {
+    passed: false,
+    error_message: "",
+  };
+  if (typeof product.title !== "string" || product.title.trim() === "") {
+    returnObj.error_message = "invalid product title";
+    return returnObj;
+  } else if (isNaN(product.price) || product.price < 1) {
+    returnObj.error_message = "invalid product price";
+    return returnObj;
+  } else if (isNaN(product.quantity)) {
+    returnObj.error_message = "invalid product quantity";
+    return returnObj;
+  }
+  returnObj.passed = true;
+  return returnObj;
+}
+
+module.exports = async (req, res, next) => {
+  const validationCheck = isValidProduct(req.body);
+  if (validationCheck.passed === true) {
+    next();
+  } else {
+    const error = new Error(validationCheck.error_message);
+    next(error);
+  }
+};
