55f7dd53e147d50204f16501cce04d4e2e4f84e0
Emmanuel Neni <emmanuelneni@gmail.com>
2020-10-08 22:46:54 +0300

    added delete button and func and margin bottom

--- client/product.js
+++ client/product.js
@@ -1,4 +1,10 @@
-import { getIdFromQuery, fetchProductById, generateContent } from "./shared.js";
+import {
+  API_URL,
+  getIdFromQuery,
+  fetchProductById,
+  generateContent,
+  alertBlock,
+} from "./shared.js";
 const productSection = document.querySelector("main section");
 
 const productId = getIdFromQuery();
@@ -6,6 +12,40 @@ const productId = getIdFromQuery();
 fetchProductById(productId).then(displayProduct);
 
 function displayProduct({ product }) {
-  const btn = `<a href="edit.html?id=${product.id}" class="btn btn-success mt-2">Edit Product</a>`;
+  const btn = `
+  <div class="row">
+    <div class="col-6"> 
+      <a href="edit.html?id=${product.id}" class="btn btn-success mt-2">
+        Edit Product
+      </a> 
+    </div>
+    <div class="col-6">
+      <button
+        id="delete"
+        class="btn btn-danger mt-2">
+        Delete Product Product
+      </button> 
+    </div>
+  </div>
+  `;
   generateContent(product, 12, btn, productSection);
+
+  const deleteBtn = document.querySelector("#delete");
+
+  deleteBtn.addEventListener("click", () => {
+    deleteProduct(productId).then(() => {
+      window.location = "/";
+    });
+  });
+}
+
+function deleteProduct(id) {
+  return fetch(`${API_URL}/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .catch((err) => {
+      alertBlock.classList.remove("d-none");
+      alertBlock.textContent = err;
+    });
 }
