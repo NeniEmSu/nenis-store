1b74a60d7414591cea8731d320675e6d71fa1a60
Emmanuel Neni <emmanuelneni@gmail.com>
2020-10-08 14:12:26 +0300

    updating db name

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      database: "nenis-store",
+      database: "nenis_store",
       user: "postgres",
       password: "neni",
     },
