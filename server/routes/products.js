53984d10454deb814547f96dbbd57735a7828fd0
Emmanuel Neni <emmanuelneni@gmail.com>
2020-10-08 17:02:43 +0300

    setting idvalidaton middleware

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const productsCtrl = require("../controllers/productsController");
+const validateId = require("../middleware/validateId");
 
 router.get("/", productsCtrl.getProducts);
 
-router.get("/:id", productsCtrl.getSingeProduct);
+router.get("/:id", validateId, productsCtrl.getSingeProduct);
 
 router.post("/", productsCtrl.addProduct);
 
-router.put("/:id", productsCtrl.updateProduct);
+router.put("/:id", validateId, productsCtrl.updateProduct);
 
 module.exports = router;
