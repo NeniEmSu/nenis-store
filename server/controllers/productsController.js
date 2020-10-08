c9ba81925aae49ff5c214bb7e595b646f91b7a01
Emmanuel Neni <emmanuelneni@gmail.com>
2020-10-08 12:34:30 +0300

    validtiond req.body and creating addProd ctrl

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -1,5 +1,24 @@
 const queries = require("../db/queries");
 
+function isValidProduct(product) {
+  let returnObj = {
+    passed: false,
+    error_message: "",
+  };
+  if (typeof product.title !== "string" || product.title.trim() === "") {
+    returnObj.error_message = "invalid product title";
+    return returnObj;
+  } else if (isNaN(product.price) || product.price < 1) {
+    returnObj.error_message = "invalid product price";
+    return returnObj;
+  } else if (isNaN(product.quantity)) {
+    returnObj.error_message = "invalid product quantity";
+    return returnObj;
+  }
+  returnObj.passed = true;
+  return returnObj;
+}
+
 exports.getProducts = async (req, res, next) => {
   try {
     const products = await queries.getAll();
@@ -26,6 +45,32 @@ exports.getSingeProduct = async (req, res, next) => {
           });
     } else {
       const error = new Error("Invalid id");
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.addProduct = async (req, res, next) => {
+  try {
+    const validationCheck = isValidProduct(req.body);
+    if (validationCheck.passed === true) {
+      const { title, description, image, price, quantity } = req.body;
+      const product = {
+        title,
+        description,
+        image,
+        price,
+        quantity,
+      };
+      const createdProductId = await queries.create(product);
+      res.status(201).json({
+        type: "success",
+        id: createdProductId,
+      });
+    } else {
+      const error = new Error(validationCheck.error_message);
       next(error);
     }
   } catch (error) {
