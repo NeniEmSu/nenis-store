755d439d94e4be780dcf7197c0f7ecb83e535be1
Emmanuel Neni <emmanuelneni@gmail.com>
2020-10-08 17:11:14 +0300

    using body validation on routes

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const productsCtrl = require("../controllers/productsController");
+const validateBody = require("../middleware/validateBody");
 const validateId = require("../middleware/validateId");
 
 router.get("/", productsCtrl.getProducts);
 
 router.get("/:id", validateId, productsCtrl.getSingeProduct);
 
-router.post("/", productsCtrl.addProduct);
+router.post("/", validateBody, productsCtrl.addProduct);
 
-router.put("/:id", validateId, productsCtrl.updateProduct);
+router.put("/:id", validateId, validateBody, productsCtrl.updateProduct);
 
 module.exports = router;
